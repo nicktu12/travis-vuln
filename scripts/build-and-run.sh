cf5c4b587743b573a8a634f736751eef9c4d96bc
Nick Teets <nick.teets@stackhawk.com>
2020-07-01 12:27:06 -0600

    tutorial

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
-# Build the vuln-django image "micro" stage and run it with Nginx and PostgreSQL
+# Build and run vuln-django with Nginx and PostgreSQL
 set -ex
+
+# We’ll use this `docker-compose exec` command a lot...
 EXEC_CMD='docker-compose --file docker-micro.yml exec vuln-django'
 
-# Build any docker images, in particular the app container
+# Build docker images, in particular the vuln-django container
 docker-compose -f docker-micro.yml build
 
-# Launch the app container with Postgres backend and Nginx frontend
+# Launch the app, PostgreSQL and Nginx
 docker-compose -f docker-micro.yml up --detach
 
-# Run data migrations, create admin account, and seed data.
-$(dirname $0)/migrations.sh
+# Wait for the database, using netcat to ping it
+echo Wait for database to become available...
+while ! ${EXEC_CMD} bash -c 'nc -z "${SQL_HOST}" "${SQL_PORT}"'; do
+  sleep 0.5
+done
+echo Database ready!
 
-# Create Django admin user
+# Run database migrations to build tables
+${EXEC_CMD} python manage.py migrate
+
+# Create Django admin user from DJANGO_SUPERUSER_ environment variables
 ${EXEC_CMD} python manage.py createsuperuser --no-input
 
 # Seed database with test data
@@ -20,3 +29,4 @@ ${EXEC_CMD} python manage.py seed polls --number=5
 
 # Run unit tests
 ${EXEC_CMD} python manage.py test
+
